a277daea66b6ebeed4fc406417905c91d3fdfe15
hieu <phamxuanhieu824@gmail.com>
2019-10-27 16:03:02 +0700

    commit message

---
+++ ave_point.rb
@@ -0,0 +1,30 @@
+puts "tên và điểm"
+name = "Jonh"
+puts name
+number = 40
+puts number
+
+name = "Paul"
+puts name
+number = 76
+puts number
+
+name = "George"
+puts name
+number=58
+puts number
+
+name = "Ringo"
+puts name
+number = 78
+puts number
+
+puts "Tổng điểm của 4 người"
+sum = 40+76+58+78
+puts sum
+puts "Điểm trung bình của 4 người"
+average = sum / 4
+puts average
+end
+
+
