9089ca607897ddeca2fbfa5260af8550b32540d4
xuanhieu <phamxuanhieu824@gmail.com>
2019-11-10 09:18:44 +0700

    Commit Message

---
+++ 01_bmi.rb
@@ -0,0 +1,21 @@
+name = "Mike"
+puts name
+height = 1.70
+weight = 65
+BMI = weight / ( height * height )
+puts "Chỉ số cơ thể = ", BMI
+
+name = "Bob"
+puts name
+height = 1.75
+weight = 95
+BMI = weight / ( height * height )
+puts "chỉ số cơ thể = ", BMI
+
+name = "Ana"
+puts name
+height = 1.55
+weight = 42
+BMI = weight / ( height * height )
+puts "chỉ sô cơ thể = " , BMI
+
---
+++ 02_Bmi_with_category.rb
@@ -0,0 +1,43 @@
+
+name = "Mike "
+puts name
+height = 1.70
+weight = 65
+BMI = weight / ( height * height )
+print "Chỉ số cơ thể = ", BMI
+if BMI < 18.5
+    puts " low"
+elsif BMI > 18.5 and BMI < 25.0
+    puts " Normal"
+else 25.0 >= BMI 
+    puts " Obese"
+end
+
+name = "Bob "
+puts name
+height = 1.75
+weight = 95
+BMI = weight / ( height * height )
+print "chỉ số cơ thể = ", BMI
+if BMI < 18.5
+   puts " low"
+elsif BMI > 18.5 and BMI < 25.0
+    puts " Normal"
+else 25.0 >= BMI 
+    puts " Obese"
+end
+
+name = "Ana "
+puts name
+height = 1.55
+weight = 42
+BMI = weight / ( height * height )
+print "chỉ sô cơ thể = " , BMI
+if BMI < 18.5
+   puts " low"
+elsif BMI > 18.5 and BMI < 25.0
+    puts " Normal"
+else 25.0 >= BMI 
+    puts " Obese"
+end
+
