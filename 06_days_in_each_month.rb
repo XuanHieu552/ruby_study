f10263fc663b69d137d9c70377767f4c9f4ea4ae
xuanhieu <phamxuanhieu824@gmail.com>
2019-11-21 17:19:20 +0700

    Bai 6

---
+++ 06_days_in_each_month.rb
@@ -0,0 +1,15 @@
+month_names = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+days = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+print month_names[0]
+print " has "
+print days[0]
+print " days"
+counter = 0
+while counter <= 10
+    counter += 1
+print "\n"
+print month_names[counter]
+print " has "
+print days[counter]
+print " days"
+end
